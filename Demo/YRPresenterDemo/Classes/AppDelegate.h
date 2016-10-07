50b78cb8fc8c8d4e645f6bc2da93884683cde2c6
Yuri Romanchenko <solomidSF@bk.ru>
2016-10-07 22:10:58 +0300

    Initial project structure

---
+++ Demo/YRPresenterDemo/Classes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YRPresenterDemo
+//
+//  Created by Yuriy Romanchenko on 10/7/16.
+//  Copyright © 2016 solomidSF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo/YRPresenterDemo/Classes/ViewControllers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YRPresenterDemo
+//
+//  Created by Yuriy Romanchenko on 10/7/16.
+//  Copyright © 2016 solomidSF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
